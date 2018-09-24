d9377412d7d6b3b523092f815cc0752997b79711
Khail Saboor <khalilsaboor1@gmail.com>
2018-09-24 15:43:03 -0400

    added more notes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
 
+    private static String str = "StarTrooper";
+
     public static void main(String[] args) {
 
         fibonacci(8);
@@ -17,18 +19,22 @@ public class Main {
          **/
         /**
          * have last and the current number be printed
-         * because the are at the start of the sequence
+         * because they are at the start of the sequence
          */
-
+            //First Number          Second Number       CurrentNumber
         int previousPreviousNumber, previousNumber = 0, currentNumber = 1;
 
         System.out.println(previousNumber);
         System.out.println(currentNumber);
         for (int j = 1; j <= numberOfFabonacciSequence; j++) {
             previousPreviousNumber = previousNumber;
+
             previousNumber = currentNumber;
+
             currentNumber = previousPreviousNumber + previousNumber;
+
             System.out.println(currentNumber);
         }
+        //System.out.println(Math.pow((1+ 5 % 2),numberOfFabonacciSequence));
     }
 }
